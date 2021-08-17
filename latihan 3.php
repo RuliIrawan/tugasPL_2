a9dc87126052e867b65b92b84dcc9206c8908745
RuliIrawan <89058349+RuliIrawan@users.noreply.github.com>
2021-08-17 20:27:27 +0700

    Update latihan 3.php

--- latihan 3.php
+++ latihan 3.php
@@ -1,4 +1,4 @@
-<h1>Edit Mahasiswa</h2>
+<h1>Edit Mahasiswa</h1>
 <a href="latihan1.php"><span>< Kembali</span></a>
 <br></br>
 <?php
@@ -6,13 +6,36 @@
     $db     = new Database();
     $con    = $db->Connect();
     $id     = $_GET['id'];
-    $data   = mysqli_query($con,"select * from mahasiswa where id='$id'");
+    $id     = 
+
+    $data   = mysqli_query($con,"SELECT * from mahasiswa where id='$id'");
     while($d = mysqli_fetch_array($data)){
 ?>
     <form action="update.php" method="POST">
-        <input type="hidden" name="id" value="<?= $d['id']; ?>">
-        <input type="text" name="npm" value="<?= $d['npm']; ?>">
-        <input type="text" name="nama" value="<?= $d['nama']; ?>">
-        <input type="submit" name="proses" value="Simpan">
+        <table>
+            <tr>
+                <td>NPM :</td>
+                <td>
+                    <input type="hidden" name="id" value="<?= $d['id']; ?>">
+                    <input type="text" name="npm" value="<?= $d['npm']; ?>">
+                </td>
+            </tr>
+            <tr>
+                <td>Nama :</td>
+                <td>
+                    <input type="text" name="nama" value="<?= $d['nama']; ?>">
+                </td>
+            </tr>    
+        <tr>
+            <td></td>
+            <td>
+                <input type="submit" value="Simpan">        
+            </td>
+        </tr>
+        
+        </table>    
     </form>
-<?php }?> 
+<?php 
+}
+
+?>
