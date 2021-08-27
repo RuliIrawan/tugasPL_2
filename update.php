d405663c69144d2ad14afb1498ab97c85d56b72a
RuliIrawan <89058349+RuliIrawan@users.noreply.github.com>
2021-08-27 23:24:34 +0700

    Add files via upload

--- update.php
+++ update.php
@@ -10,5 +10,9 @@
     
     mysqli_query($con,"UPDATE mahasiswa set npm='$npm', nama='$nama' where id='$id'");
     header("location:latihan1.php");
-
-?>
+    
+    $id_dosen    = $_POST['id_dosen'];
+    $kode_dosen  = $_POST['kode_dosen'];
+    $nama_dosen  = $_POST['nama_dosen'];
+    mysqli_query($con,"UPDATE dosen set kode_dosen='$kode_dosen', nama_dosen='$nama_dosen' where id_dosen='$id_dosen'");
+    header("location:latihan5.php");
